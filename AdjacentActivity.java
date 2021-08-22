72a135cab044619d5cdbdcfadbaeca5cf7ed2adb
Deved Roy <44873540+devedroy@users.noreply.github.com>
2021-08-22 19:33:55 +0530

    update AdjacentActivity

--- AdjacentActivity.java
+++ AdjacentActivity.java
@@ -3,6 +3,7 @@ package com.example.androidmultiwindowfeature;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class AdjacentActivity extends AppCompatActivity {
 
@@ -10,5 +11,23 @@ public class AdjacentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adjacent);
+
+        if (isInMultiWindowMode()){
+            Toast.makeText(this, "I was launched in Multi-window mode", Toast.LENGTH_LONG).show();
+        }
+        else {
+            Toast.makeText(this, "I wasn't launched in Multi-window mode", Toast.LENGTH_LONG).show();
+        }
     }
+
+    @Override
+    public void onMultiWindowModeChanged(boolean isInMultiWindowMode) {
+        super.onMultiWindowModeChanged(isInMultiWindowMode);
+
+        if(isInMultiWindowMode) {
+            Toast.makeText(this, "I am in Multi-window mode", Toast.LENGTH_LONG).show();
+        }
+        else {
+            Toast.makeText(this, "I am not in Multi-window mode", Toast.LENGTH_LONG).show();
+        }
 }
